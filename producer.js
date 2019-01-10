0e33f6746176bf976dd427e09fc42e145af0d7f7
Vladimir Nevyhosteny <vnevyhosteny@gmail.com>
2019-01-10 15:38:26 +0100

    Convert to localhosted Kafka
     - overall simplify

--- producer.js
+++ producer.js
@@ -1,28 +1,21 @@
-const Kafka = require("node-rdkafka");
+import { Producer } from "node-rdkafka";
 
-const kafkaConf = {
-  "group.id": "cloudkarafka-example",
-  "metadata.broker.list": process.env.CLOUDKARAFKA_BROKERS.split(","),
-  "socket.keepalive.enable": true,
-  "security.protocol": "SASL_SSL",
-  "sasl.mechanisms": "SCRAM-SHA-256",
-  "sasl.username": process.env.CLOUDKARAFKA_USERNAME,
-  "sasl.password": process.env.CLOUDKARAFKA_PASSWORD,
-  "debug": "generic,broker,security"
-};
+const topic = 'TopicOne';
 
-const prefix = process.env.CLOUDKARAFKA_TOPIC_PREFIX;
-const topic = `${prefix}.test`;
-const producer = new Kafka.Producer(kafkaConf);
+const producer = new Producer({
+  'metadata.broker.list': 'localhost:9092'
+});
+
 const maxMessages = 20;
-
 const genMessage = i => new Buffer(`Kafka example, message number ${i}`);
 
 producer.on("ready", function(arg) {
   console.log(`producer ${arg.name} ready.`);
+
   for (var i = 0; i < maxMessages; i++) {
     producer.produce(topic, -1, genMessage(i), i);
   }
+
   setTimeout(() => producer.disconnect(), 0);
 });
 
@@ -34,7 +27,9 @@ producer.on('event.error', function(err) {
   console.error(err);
   process.exit(1);
 });
+
 producer.on('event.log', function(log) {
   console.log(log);
 });
+
 producer.connect();
